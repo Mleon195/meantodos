4d24124678649734c4605b514de4bf0abf889274
Mleon195 <mleon@mail.ccsf.edu>
2017-07-25 21:21:34 -0700

    set up angular

--- server.js
+++ server.js
@@ -11,9 +11,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'client')));
+
+
 //middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+
+
 
 //routes
 app.use('/', index);
