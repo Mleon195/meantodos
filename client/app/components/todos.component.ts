5a336dce39817149accb6e10ba24049b7bc5f78a
Mleon195 <mleon@mail.ccsf.edu>
2017-07-27 16:26:48 -0700

    update todo

--- client/app/components/todos.component.ts
+++ client/app/components/todos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import {TodoService} from '../services/todo.service';
 import {Todo} from '../Todo';
 
@@ -8,18 +8,54 @@ import {Todo} from '../Todo';
   templateUrl: 'todos.component.html'
 })
 
-export class TodosComponent implements OnInit { 
+export class TodosComponent implements OnInit {
   todos: Todo[];
-
+  
   constructor(private _todoService: TodoService){
-
+    
   }
-
+  
   ngOnInit(){
     this.todos = [];
     this._todoService.getTodos()
       .subscribe(todos => {
-        console.log(todos)
+        this.todos = todos;
       });
   }
+
+  addTodo(event, todoText){
+    var result;
+    var newTodo = {
+      text: todoText.value,
+      isCompleted: false
+    };
+
+    result = this._todoService.saveTodo(newTodo);
+    result.subscribe(x => {
+      this.todos.push(newTodo);
+      todoText.value = '';
+    });
+  }
+
+  setEditState(todo, state){
+    if(state){
+      todo.isEditMode = state;
+    } else {
+      delete todo.isEditMode;
+    }
+  }
+
+  updateStatus(todo){
+    var _todo = {
+      _id:todo._id,
+      text: todo.text,
+      isCompleted: !todo.isCompleted
+    };
+
+    this._todoService.updateTodo(_todo)
+      .subscribe(data => {
+        todo.isCompleted = !todo.isCompleted;
+      });
+    }
+
 }
